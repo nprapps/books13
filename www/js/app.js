0effeb0e983ae96847c09ba5840bd18f509f30bb
Jeremy Bowers <jeremyjbowers@gmail.com>
2013-11-06 16:01:43 -0500

    Sets up a basic book card for Danny and Shelly to make un-terrible.

--- www/js/app.js
+++ www/js/app.js
@@ -1,2 +1,20 @@
 $(function() {
+    // Variables and CONSTANTS.
+    var $books_target = $('#books-target');
+
+    // Utility functions.
+    var load_books = function(book_list) {
+        _.each(book_list, function(element, index, list){
+            $books_target.append(JST.book_card({book:element}));
+        });
+    };
+
+    // Stuff that runs on the page load.
+    var init = function() {
+        load_books(BOOKS);
+    };
+
+    // Run on page load.
+    // Typically, we put all this in init().
+    init();
 });
