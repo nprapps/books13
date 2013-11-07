aed2ab3e88602d491f2f07a3037b1a2455743ebd
Christopher Groskopf <staringmonkey@gmail.com>
2013-11-07 13:44:48 -0600

    Tag hashes. Closes #72.

--- www/js/app.js
+++ www/js/app.js
@@ -28,26 +28,56 @@ var load_books = function(book_list) {
 /*
  * Filter the list to a certain tag.
  */
-var on_tag_clicked = function(){
+var on_tag_clicked = function() {
     var tag = $(this).data('tag-slug');
-    hasher.setHash(tag);
+    hasher.setHash('tag/' + tag);
+
+    return false;
 };
 
 /*
- * Respond to url changes.
+ * New tag hash url.
  */
-var on_hash_changed = function(new_hash, old_hash){
+var on_tag_hash = function(slug) {
     var book_list = BOOKS;
 
-    if (new_hash != '') {
-        book_list = _.filter(book_list, function(book){
-            return book.tags.indexOf(new_hash) >= 0;
+    if (slug != '') {
+        book_list = _.filter(book_list, function(book) {
+            return book.tags.indexOf(slug) >= 0;
         });
     }
 
     load_books(book_list);
-
+    
     smooth_scroll($content, 0);
+
+};
+
+/*
+ * New book hash url.
+ */
+var on_book_hash = function(slug) {
+    // Clear current tag filter
+    on_tag_hash('');
+
+    console.log('jump to book: ' + slug);
+}
+
+/*
+ * Respond to url changes.
+ */
+var on_hash_changed = function(new_hash, old_hash) {
+    var bits = new_hash.split('/');
+    var hash_type = bits[0];
+    var hash_slug = bits[1];
+    
+    if (hash_type == 'tag') {
+        on_tag_hash(hash_slug);
+    } else if (hash_type == 'book') {
+        on_book_hash(hash_slug);
+    }
+
+    return false;
 };
 
 $(function() {
