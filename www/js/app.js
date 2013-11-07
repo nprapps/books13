eb46939c632be6cb97c7460e8ac9f96594f9ffd4
Jeremy Bowers <jeremyjbowers@gmail.com>
2013-11-07 14:37:53 -0500

    Mid-way commit for modal. Will merge with Chris's code.

--- www/js/app.js
+++ www/js/app.js
@@ -1,35 +1,66 @@
 var $books_target;
 
 var load_books = function(book_list) {
+
     $books_target.empty();
 
     _.each(book_list, function(element, index, list){
+
         $books_target.append(JST.book_card({
             book: element,
             app_config: APP_CONFIG
         }));
+
     });
+
 };
 
 var on_tag_clicked = function(){
+
+    // Sets up the tag slug we can use in the URL.
     var tag = $(this).data('tag-slug');
+
+    // Post this tag hash to the URL and let
+    // hasher handle the rest.
     hasher.setHash(tag);
 };
 
 var smooth_scroll = function(offset_element, padding) {
+
+    // Calculate the element against the top of the page.
     var elementOffset = offset_element.offset().top;
+
+    // Subtract the padding.
     var distance =  elementOffset - padding;
+
+    // Scroll exactly that far. And make it all animate-y.
     $('body').animate({ scrollTop: distance + "px" });
+
 };
 
 var on_hash_changed = function(new_hash, old_hash){
-    var book_list = _.filter(BOOKS, function(book){
-        return book.tags.indexOf(new_hash) >= 0;
-    });
+    // If the hash isn't blank, filter.
+    if (new_hash !== '') {
 
-    load_books(book_list);
+        // Set up a filtered list of all books.
+        var book_list = _.filter(BOOKS, function(book){
 
-    smooth_scroll($('#content'), 0);
+            // Specifically, filter on the tag.
+            return book.tags.indexOf(new_hash) >= 0;
+        });
+
+        // Load our filtered subset of books.
+        load_books(book_list);
+
+        // Scroll to the top of the #content div.
+        smooth_scroll($('#content'), 0);
+
+    } else {
+
+        // Otherwise, just load everything.
+        load_books(BOOKS);
+
+    }
 };
 
 $(function() {
@@ -39,10 +70,8 @@ $(function() {
     // Event handlers.
     $('body').on('click', 'button.tag', on_tag_clicked);
 
+    // Set up the hasher bits to grab the URL hash.
     hasher.changed.add(on_hash_changed);
     hasher.initialized.add(on_hash_changed);
     hasher.init();
-
-    load_books(BOOKS);
-
 });
